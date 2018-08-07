40ac5dab4f259d21d3bdd4616302342aa8e43a83
Quispie <wesley.quispel@gmail.com>
2018-08-07 12:41:52 +0100

    update server

--- server/src/app.js
+++ server/src/app.js
@@ -2,20 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-// const {sequelize} = require('./models')
-// const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-// require('./passport')
+app.get('/status', (req, res) => {
+	res.send({
+		message: 'hello world!'
+	})
+})
 
-// require('./routes')(app)
-
-// sequelize.sync({force: false})
-//   .then(() => {
-//     app.listen(config.port)
-//     console.log(`Server started on port ${config.port}`)
-// })
+app.list(process.env.PORT || 8081)
