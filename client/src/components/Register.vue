bf3b16b43c46e4a1af3e11fa54e9f1be4e4ed075
Quispie <wesley.quispel@gmail.com>
2018-08-07 13:34:12 +0100

    Update gitignore

---
+++ client/src/components/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="register">
+    <h1> Register </h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="Email"
+    >>
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="Password"
+    >>
+    <br>
+    <button>Register</button>>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('Email has changed', value)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
